5c73c96b81662fa1027990538a21c0ff8c468758
CimimUxMaio <cimimuxmaio@gmail.com>
2022-01-22 03:54:15 -0300

    Print user produced exceptions

--- src/Discord/Core/EventLoop.hs
+++ src/Discord/Core/EventLoop.hs
@@ -5,18 +5,23 @@ import Control.Monad.IO.Class (MonadIO(liftIO))
 import Control.Concurrent (readChan, Chan)
 import Discord.API.Internal.Types.BotEvent (BotEvent)
 import Discord.Core.Internal.Types (BotApp(BotApp, appConfig, appInitialState), runBotAction, botAppEventHandler)
+import Control.Exception (catch, SomeException)
 
 
 
 startEventLoop :: Chan BotEvent -> BotApp s -> IO ()
 startEventLoop eventQueue app = loop eventQueue eventHandler initialState
-    where eventHandler currentState = runBotAction cfg currentState . botAppEventHandler app
+    where eventHandler currentState event = snd <$> (runBotAction cfg currentState . botAppEventHandler app $ event)
           cfg = appConfig app
           initialState = appInitialState app
 
 
-loop :: Chan BotEvent -> (s -> BotEvent -> IO ((), s)) -> s -> IO ()
+loop :: Chan BotEvent -> (s -> BotEvent -> IO s) -> s -> IO ()
 loop eventQueue eventHandler currentState = do
     event <- readChan eventQueue
-    (_, finalState) <- eventHandler currentState event
+    finalState <- catch (eventHandler currentState event) (handleException currentState)
     loop eventQueue eventHandler finalState
+
+
+handleException :: s -> SomeException -> IO s  -- Handles user produced exceptions
+handleException prevState e = print e >> pure prevState
