e256f970db1633e988d2aee8f79ba664b1b4c480
CimimUxMaio <cimimuxmaio@gmail.com>
2022-02-28 14:29:22 -0300

    Refactor eventLoop

--- src/Discord/Core/Internal/EventLoop.hs
+++ src/Discord/Core/Internal/EventLoop.hs
@@ -9,28 +9,33 @@ import Discord.API.Internal.Types.BotEvent (BotEvent)
 import Control.Exception (catch, SomeException (SomeException), catches, Handler (Handler))
 import Control.Monad.RWS (MonadState(put, get), void, forever)
 import Control.Monad.Trans.State (StateT(runStateT))
-import Discord.Core.Internal.Types (BotApp (appConfig, appInitialState, appExceptionHandlers), runBotAction, runBotAction_)
+import Discord.Core.Internal.Types (BotApp (appConfig, appInitialState, appExceptionHandlers), runBotAction, runBotAction_, BotConfig)
 import Discord.Core.Internal.Utils
     ( toHandler, botAppEventHandler )
 import Data.Bifunctor (second)
+import Discord.Core.Context (Context)
 
 
 startEventLoop :: Chan BotEvent -> BotApp s -> IO ()
 startEventLoop eventQueue app = void . (`runStateT` initialState) . forever $ do
     event <- liftIO $ readChan eventQueue
     currentState <- get
+    let cfg = appConfig app
     let (ctx, action) = botAppEventHandler app event
-    finalState <- liftIO $ catches (runBotAction_ cfg currentState action) (exceptionHandlers currentState ctx)
+    let runEvent = runBotAction_ cfg currentState action
+    finalState <- liftIO $ catches runEvent (exceptionHandlers app currentState ctx)
     put finalState
 
-    where eventHandler = botAppEventHandler app
-          cfg = appConfig app
-          initialState = appInitialState app
-          userExceptionHandlers currentState currentCtx = map (toHandler cfg currentState currentCtx) (appExceptionHandlers app)
-          exceptionHandlers currentState currentCtx = 
-              userExceptionHandlers currentState currentCtx ++ [defaultExceptionHandler currentState]
+    where initialState = appInitialState app
 
 
 defaultExceptionHandler :: s -> Handler s
 defaultExceptionHandler prevState = 
     Handler $ \(e :: SomeException) -> print e >> pure prevState
+
+
+exceptionHandlers :: BotApp s -> s -> Context -> [Handler s]
+exceptionHandlers app currentState currentCtx = 
+    userExceptionHandlers ++ [defaultExceptionHandler currentState]
+    where userExceptionHandlers = map (toHandler cfg currentState currentCtx) (appExceptionHandlers app)
+          cfg = appConfig app
